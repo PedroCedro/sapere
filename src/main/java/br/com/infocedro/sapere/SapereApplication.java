bc7cbe055a05d79db0ce52264df64af1e3612345
Pedro Cedro <phcedro84@gmail.com>
2026-03-10 11:37:18 -0300

    Criado o cod base. Criado o repo no github.

---
+++ src/main/java/br/com/infocedro/sapere/SapereApplication.java
@@ -0,0 +1,13 @@
+package br.com.infocedro.sapere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infocedro/sapere/SapereApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infocedro.sapere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
